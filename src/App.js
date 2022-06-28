3f431a6406caec6a8e2340e561cdc2c1acf5d43d
VaibhavB83 <vaibhav161971@gmail.com>
2022-06-28 10:51:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css';
+import Free123 from './Free123';
+import Plus123 from './Plus123';
+import Pro123 from './Pro123';
+
+const App = () => {
+
+    return <div>
+                 <Free123 /><Plus123 /><Pro123 />
+                 
+    
+    
+    
+    
+    </div>
+                
+};
+
+export default App;
